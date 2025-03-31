8899fcfdc0a541d0346fddcae1d599ae794ad6b1
noeuncloud <noeun1101@gmail.com>
2025-03-31 17:16:33 +0900

    Add files via upload

---
+++ calculator_basic.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter num1 : "))
+num2 = int(input("Enter num2 : "))
+
+print("[Basic Calaulator]")
+print(num1,"+",num2,"=",num1+num2)
+print(num1,"-",num2,"=",num1-num2)
+print(num1,"*",num2,"=",num1*num2)
+print(num1,"//",num2,"=",num1//num2)
+print(num1,"/",num2,"=",num1/num2)
+print(num1,"%",num2,"=",num1%num2)
+print(num1,"**",num2,"=",num1**num2)
---
+++ get_ave.py
@@ -0,0 +1,13 @@
+s1= float(input("Enter score 1 :"))
+s2= float(input("Enter score 2 :"))
+s3= float(input("Enter score 3 :"))
+s4= float(input("Enter score 4 :"))
+s5= float(input("Enter score 5 :"))
+
+print("[Result]")
+
+total = s1+s2+s3+s4+s5
+print("Total score : ",total)
+
+ave= total/5
+print("Average score :",ave)
