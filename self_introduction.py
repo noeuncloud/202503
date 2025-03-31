4338d485b82119d80e4ffa1dbda493d5341b598a
noeuncloud <noeun1101@gmail.com>
2025-03-31 17:15:03 +0900

    Add files via upload

---
+++ self_introduction.py
@@ -0,0 +1,8 @@
+name=input("이름 : ")
+major=input("전공 : ")
+num=int(input("학번 : "))
+year = input("생년 월일 : ")
+
+print("[자기소개]")
+print("숙명여대",major,num,"학번",name,"입니다.")
+print("제 생년월일은",year,"입니다.")
