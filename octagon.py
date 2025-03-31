afab6c2b33b3bc6befc7adae3eaf691fae7ef6d3
noeuncloud <noeun1101@gmail.com>
2025-03-31 17:15:45 +0900

    Add files via upload

---
+++ octagon.py
@@ -0,0 +1,22 @@
+import turtle
+
+t=turtle.Pen()
+t.shape("turtle")
+t.color("purple")
+
+t.forward(100)
+t.left(45)
+t.forward(100)
+t.left(45)
+t.forward(100)
+t.left(45)
+t.forward(100)
+t.left(45)
+t.forward(100)
+t.left(45)
+t.forward(100)
+t.left(45)
+t.forward(100)
+t.left(45)
+t.forward(100)
+t.left(45)
---
+++ triangle.py
@@ -0,0 +1,14 @@
+import turtle
+
+t=turtle.Pen()
+t.shape("turtle")
+t.color("green")
+
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+
+
